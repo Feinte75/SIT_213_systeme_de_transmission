96d670bbcd859493bf221287ee21a0ecf489070a
feinte75 <fintin@free.fr>
2014-10-06 22:28:58 +0200

    Ajout commentaire et javadoc pour l'emetteur et le recepteur

--- src/element_transmission/EmetteurNumeriqueAnalogique.java
+++ src/element_transmission/EmetteurNumeriqueAnalogique.java
@@ -35,9 +35,12 @@ public class EmetteurNumeriqueAnalogique extends Transmetteur<Boolean, Float>
 			codageNRZT();
 			break;
 		}
-
 	}
 
+	/**
+	 * Codage retour à zéro. Signal à min ou max pendant 1/2 symbole puis 0
+	 * ensuite
+	 */
 	public void codageRZ() {
 
 		for (int i = 0; i < informationRecue.nbElements(); i++) {
@@ -59,13 +62,15 @@ public class EmetteurNumeriqueAnalogique extends Transmetteur<Boolean, Float>
 		}
 	}
 
+	/**
+	 * Codage Non Retour à Zéro. Signal à max pour un "1" et à min pour un "0"
+	 */
 	public void codageNRZ() {
 
 		for (int i = 0; i < informationRecue.nbElements(); i++) {
-			// System.out.println("info re�ue : "+
-			// informationRecue.nbElements());
+
 			for (int j = 0; j < nbEchantillon; j++) {
-				// System.out.println("i : "+i+"  j : "+j);
+
 				if (informationRecue.iemeElement(i))
 					informationEmise.add(max);
 				else
@@ -74,6 +79,11 @@ public class EmetteurNumeriqueAnalogique extends Transmetteur<Boolean, Float>
 		}
 	}
 
+	/**
+	 * Codage Non Retour à Zéro Triangulaire. Pour un "1", pente montante de min
+	 * à max durant 1/3 de la période, plateau à max pendant 1 autre tier puis
+	 * pente descendante jusqu'à min pendant le dernier tier de la période
+	 */
 	public void codageNRZT() {
 
 		float penteMontante = 0.0f;
@@ -81,22 +91,26 @@ public class EmetteurNumeriqueAnalogique extends Transmetteur<Boolean, Float>
 
 		for (int i = 0; i < informationRecue.nbElements(); i++) {
 
-			if (informationRecue.iemeElement(i)) {
+			if (informationRecue.iemeElement(i)) { // cas d'un "1"
+				// Pente montante
 				for (int j = 0; j < nbEchantillon / 3; j++) {
 					penteMontante = ((max - min) * 3 / nbEchantillon) * j;
 					informationEmise.add(penteMontante);
 				}
 
+				// Plateau
 				for (int j = nbEchantillon / 3; j < 2 * nbEchantillon / 3; j++) {
 					informationEmise.add(max);
 				}
 
+				// Pente descendante
 				for (int j = 2 * nbEchantillon / 3; j < nbEchantillon; j++) {
 					penteDescendante = ((min - max) * 3 / nbEchantillon)
 							* (j - 2 * nbEchantillon / 3) + max;
 					informationEmise.add(penteDescendante);
 				}
 			} else
+				// Cas d'un "0"
 				for (int j = 0; j < nbEchantillon; j++) {
 					informationEmise.add(min);
 				}
--- src/element_transmission/RecepteurAnalogiqueNumerique.java
+++ src/element_transmission/RecepteurAnalogiqueNumerique.java
@@ -1,15 +1,17 @@
 package element_transmission;
+
 import ihm.TypeCodage;
 import exception.InformationNonConforme;
 
-
-public class RecepteurAnalogiqueNumerique extends Transmetteur<Float, Boolean> implements Convertisseur{
+public class RecepteurAnalogiqueNumerique extends Transmetteur<Float, Boolean>
+		implements Convertisseur {
 
 	private Float min, max;
 	private int nbEchantillon;
 	private TypeCodage type;
 
-	public RecepteurAnalogiqueNumerique(float min, float max, int nbEchantillon, TypeCodage type) {
+	public RecepteurAnalogiqueNumerique(float min, float max,
+			int nbEchantillon, TypeCodage type) {
 		super();
 		informationEmise = new Information<Boolean>();
 		this.min = new Float(min);
@@ -26,60 +28,69 @@ public class RecepteurAnalogiqueNumerique extends Transmetteur<Float, Boolean> i
 		case RZ:
 			decodageRZ();
 			break;
-		case NRZ: 
+		case NRZ:
 			decodageNRZ();
 			break;
 		case NRZT:
 			decodageNRZT();
 			break;
 		}
-
 	}
 
+	/**
+	 * Decodage Non Retour à Zéro Triangulaire. Instant de décision tout les
+	 * nbEchantillon/2 (milieu du symbole)
+	 */
 	private void decodageNRZT() {
 
-		for(int i=nbEchantillon/2; i<informationRecue.nbElements();i+= nbEchantillon){
+		for (int i = nbEchantillon / 2; i < informationRecue.nbElements(); i += nbEchantillon) {
 
-				if(informationRecue.iemeElement(i/2).floatValue()==max){
-					informationEmise.add(new Boolean(true));
-				}
-				else {
-					informationEmise.add(new Boolean(false));
-				}
+			if (informationRecue.iemeElement(i / 2).floatValue() == max) {
+				informationEmise.add(new Boolean(true));
+			} else {
+				informationEmise.add(new Boolean(false));
+			}
 		}
-
 	}
 
+	/**
+	 * Decodage Non Retour à Zéro. Moyenne sur l'ensemble des valeurs des
+	 * echantillons puis determination de l'etat du bit par un seuillage si la
+	 * moyenne est > à (min + max) / 2 on detecte le bit comme etant true sinon
+	 * false
+	 */
 	private void decodageNRZ() {
 
 		float somme = 0;
-		for(int i = 0; i < informationRecue.nbElements(); i+= nbEchantillon){
+		for (int i = 0; i < informationRecue.nbElements(); i += nbEchantillon) {
 
-			for(int j = i; j < i + nbEchantillon; j++){
+			for (int j = i; j < i + nbEchantillon; j++) {
 
 				somme += informationRecue.iemeElement(j).floatValue();
 			}
 			somme /= nbEchantillon;
-			if(somme >= (min + max)/2 ) informationEmise.add(new Boolean(true));
-			else informationEmise.add(new Boolean(false));
+			if (somme >= (min + max) / 2)
+				informationEmise.add(new Boolean(true));
+			else
+				informationEmise.add(new Boolean(false));
 			somme = 0;
 		}
-
 	}
 
+	/**
+	 * Decodage Retour à Zéro. Instant de décision tout les nbEchantillon/4
+	 * (quart du symbole)
+	 */
 	private void decodageRZ() {
 
-		for(int i=nbEchantillon/4; i<informationRecue.nbElements();i+= nbEchantillon){
+		for (int i = nbEchantillon / 4; i < informationRecue.nbElements(); i += nbEchantillon) {
 
-			if(informationRecue.iemeElement(i).floatValue()==max){
+			if (informationRecue.iemeElement(i).floatValue() == max) {
 				informationEmise.add(new Boolean(true));
-			}
-			else {
+			} else {
 				informationEmise.add(new Boolean(false));
 			}
-	}
-
-
+		}
 	}
 
 	@Override
@@ -93,8 +104,5 @@ public class RecepteurAnalogiqueNumerique extends Transmetteur<Float, Boolean> i
 	public void recevoir(Information<Float> information)
 			throws InformationNonConforme {
 		informationRecue = information;
-
 	}
-
-
 }
